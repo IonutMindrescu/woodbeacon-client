a5f25d6767138fd2ae57e97152573d512484ea67
WoodBeacon Device <woodbeacon@device>
2019-11-28 15:26:36 +0000

    Added sound detection by Tudor.

--- test.py
+++ test.py
@@ -1,42 +1,53 @@
+#!/usr/bin/env python3
+"""
+    Wood Beacon - Alpha 1.1a (sound recognition & alerting)
+"""
 import logging
-import json
-import numpy as np
-import RPi.GPIO as GPIO
-
 from time import sleep
+import RPi.GPIO as GPIO
 from dragino import Dragino
-from keras.preprocessing.image import img_to_array, load_img
+import json
 
+import numpy as np
+import sounddevice
+import librosa.display, os, gc
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from audio_recognition import *
 
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array, load_img
+
 GPIO.setwarnings(False)
-chainsaw_model = AudioModel("chainsaw.json", "chainsaw.h5")
+
+data = {"action": "sound-detected", "battery": 27, "sound": "drujba", "lat": 47.640173, "lng": 26.258861}
 
 D = Dragino("dragino.ini", logging_level=logging.DEBUG)
 D.join()
-
-#data = {"action": "sound-detected", "state": True, "battery": 27.0}
-data = {"action": "sound-detected", "battery": 27, "sound": "chainsaw", "lat": 47.640173, "lng": 26.258861}
-
 while not D.registered():
     print("Waiting")
     sleep(2)
 #sleep(10)
 
-def main():
-    while not D.registered():
-        print("Waiting")
-        sleep(2)
+duration = 3 #seconds
+sample_rate=22400 #frequency
 
+chainsaw_model = AudioModel("chainsaw.json", "chainsaw.h5")
+
+def main():
+    number_of_detection = 0
     while True:
         get_sound()
         img = img_to_array(load_img('rec.png', target_size=(100, 100)))
         img = np.expand_dims(img, axis=0)
         predict = chainsaw_model.predict(img)[0][0]
         print(predict)
-        if predict >= 0.9999 and predict <= 1.0:
+        if predict >= 0.999: #predict == 1.0:
+             number_of_detection += 1
+        if number_of_detection == 2:
             D.send(json.dumps(data))
             print("Beacon has been sent!")
+            number_of_detection = 0
 
 if __name__ == "__main__":
     main()
